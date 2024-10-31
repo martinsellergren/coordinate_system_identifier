a89c2130f2f8168d63a48c22a3389e889a4088f3
martinsellergren <martin.sellergren@gmail.com>
2024-10-31 17:45:39 +0100

    coordinates_opener: Add map keys

--- coordinates_opener/ios/Runner/AppDelegate.swift
+++ coordinates_opener/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBnnmHNmGs2mAPeV8rNQNT93X6duKUaAFU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
