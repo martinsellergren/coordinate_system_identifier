e1db1d120b4300f8e1ee959be6f549a4d8547c06
martinsellergren <martin.sellergren@gmail.com>
2024-11-04 13:43:50 +0100

    sql playground

---
+++ playground.sql
@@ -0,0 +1,25 @@
+SELECT distinct
+    coord_ref_sys_kind
+FROM
+    "epsg_coordinatereferencesystem";
+
+SELECT distinct
+    coord_sys_type
+FROM
+    "epsg_coordinatesystem"
+LIMIT
+    100;
+
+select distinct
+    format(
+        'https://epsg.io/%s',
+        rs.coord_ref_sys_code
+    ),
+    rs.coord_ref_sys_name,
+    rs.coord_ref_sys_kind,
+    cs.coord_sys_type
+from
+    epsg_coordinatereferencesystem rs
+    join epsg_coordinatesystem cs on rs.coord_sys_code = cs.coord_sys_code
+where
+    coord_ref_sys_kind = 'projected';
