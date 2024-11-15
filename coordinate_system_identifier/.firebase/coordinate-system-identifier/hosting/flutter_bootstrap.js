feaa6adb75027263893642c22530b3b73c8c1311
martinsellergren <martin.sellergren@gmail.com>
2024-11-15 13:30:50 +0100

    Deploy updated coordinate_system_identifier

--- coordinate_system_identifier/.firebase/coordinate-system-identifier/hosting/flutter_bootstrap.js
+++ coordinate_system_identifier/.firebase/coordinate-system-identifier/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1058456206"
+    serviceWorkerVersion: "2294947903"
   }
 });
