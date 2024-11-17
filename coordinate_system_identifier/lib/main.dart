e1990fd3064159cd39f1e97961fff90e69f46032
martinsellergren <martin.sellergren@gmail.com>
2024-11-17 08:59:15 +0100

    Add firebase analytics and crashlytics

--- coordinate_system_identifier/lib/main.dart
+++ coordinate_system_identifier/lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shared/geoutils/get_point_details.dart';
+import 'package:shared/setup_firebase.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await setupFirebase(options: DefaultFirebaseOptions.currentPlatform);
   await setupGetPointDetails();
   runApp(const App());
 }
---
+++ packages/shared/lib/setup_firebase.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+
+Future<void> setupFirebase({required FirebaseOptions options}) async {
+  await Firebase.initializeApp(options: options);
+  FirebaseAnalytics.instance; // trigger default reporting(?)
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+}
