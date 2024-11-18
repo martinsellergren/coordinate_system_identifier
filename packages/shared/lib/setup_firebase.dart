4db4b44b5038181211c82b657b983ceb731ec438
martinsellergren <martin.sellergren@gmail.com>
2024-11-18 10:34:49 +0100

    Fix firebase analytics
    There was no analytics events before. Findings (on web):
    - If not calling analytics.logEvent() etc explicitly, default events are not logged either.
    - Works if explicitly calling setAnalyticsCollectionEnabled(true).

--- packages/shared/lib/setup_firebase.dart
+++ packages/shared/lib/setup_firebase.dart
@@ -5,7 +5,7 @@ import 'package:flutter/foundation.dart';
 
 Future<void> setupFirebase({required FirebaseOptions options}) async {
   await Firebase.initializeApp(options: options);
-  FirebaseAnalytics.instance; // trigger default reporting(?)
+  await FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
   if (kIsWeb) {
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
     PlatformDispatcher.instance.onError = (error, stack) {
